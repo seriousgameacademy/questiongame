dbe20d96564fc15d2a74451efcf201c0823732eb
Ramon Huijg <ryhuijg@outlook.com>
2018-08-07 17:56:56 +0000

    createserver

--- QuestionGame/server.js
+++ QuestionGame/server.js
@@ -1,71 +1,64 @@
 'use strict';
+var http = require('http');
 var app = require('express')();
-var http = require('http').Server(app);
-//var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.listen(port, function () {
-    console.log('listening on localhost:' + port);
-});
-
-
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
-
-
-
-
-
-
-//var port = process.env.PORT || 1337;
-//var io = require('socket.io')(http);
-
-//app.get('/', function (req, res) {
-//    res.sendfile('index.html');
+http.createServer(app).listen(port);
+//http.listen(1337, function () {
+//    console.log('listening on localhost:3000');
 //});
 
-//var clients = 0;
-//var users = [];
 
-////io.on('connection', function (socket) {
-////    console.log('A user connected');
 
-////    socket.on('setUsername', function (username) {
-////        socket.username = username;
+//var app = require('express')();
+//var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-////        console.log(socket.username);
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
 
-////        if (users.indexOf(socket.username) > -1) {
-////            socket.emit('userExists', username + ' naam bestaat al! Probeer een andere naam.');
-////        } else {
-////            users.push(socket.username);
-////            clients++;
-////            socket.emit('userSet', { username: socket.username });
-////            socket.emit('welkommsg', { message: ('Welkom ' + socket.username + '!') });
-////            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' doet ook mee!') });
+var clients = 0;
+var users = [];
 
-////        }
+io.on('connection', function (socket) {
+    console.log('A user connected');
 
-////        socket.on('disconnect', function () {
-////            clients--;
+    socket.on('setUsername', function (username) {
+        socket.username = username;
 
-////            var index = users.indexOf(socket.username);
+        console.log(socket.username);
 
-////            if (index != -1) {
+        if (users.indexOf(socket.username) > -1) {
+            socket.emit('userExists', username + ' naam bestaat al! Probeer een andere naam.');
+        } else {
+            users.push(socket.username);
+            clients++;
+            socket.emit('userSet', { username: socket.username });
+            socket.emit('welkommsg', { message: ('Welkom ' + socket.username + '!') });
+            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' doet ook mee!') });
 
-////                users.splice(index, 1);
-////            }
+        }
 
-////            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' is gestopt met het spel!') });
-////        });
-////    });
+        socket.on('disconnect', function () {
+            clients--;
 
-////    socket.on('msg', function (data) {
-////        //Send message to everyone
-////        console.log(data);
-////        io.sockets.emit('newmsg', data);
-////    })
-////});
+            var index = users.indexOf(socket.username);
+
+            if (index != -1) {
+
+                users.splice(index, 1);
+            }
+
+            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' is gestopt met het spel!') });
+        });
+    });
+
+    socket.on('msg', function (data) {
+        //Send message to everyone
+        console.log(data);
+        io.sockets.emit('newmsg', data);
+    })
+});
+
 
