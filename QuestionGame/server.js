e8900a20069d770ca3e03a0a86c629dbec436e50
Ramon Huijg <ryhuijg@outlook.com>
2018-08-07 19:13:41 +0000

    app 3

--- QuestionGame/server.js
+++ QuestionGame/server.js
@@ -2,8 +2,13 @@
 var app = require('express')();
 var path = require('path');
 var port = process.env.PORT || 1337;
+var server = require('http').createServer(app);
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+});
