14c5b8798a4d8dc6e2a4563221affa7a17d95a05
Ramon Huijg <ryhuijg@outlook.com>
2018-08-07 19:50:26 +0000

    Initail Chat App

--- QuestionGame/server.js
+++ QuestionGame/server.js
@@ -22,3 +22,49 @@ var server = require('http').createServer(function (req, res) {
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
 });
+
+var io = require('socket.io')(server);
+
+
+var clients = 0;
+var users = [];
+
+io.on('connection', function (socket) {
+    console.log('A user connected');
+
+    socket.on('setUsername', function (username) {
+        socket.username = username;
+
+        console.log(socket.username);
+
+        if (users.indexOf(socket.username) > -1) {
+            socket.emit('userExists', username + ' naam bestaat al! Probeer een andere naam.');
+        } else {
+            users.push(socket.username);
+            clients++;
+            socket.emit('userSet', { username: socket.username });
+            socket.emit('welkommsg', { message: ('Welkom ' + socket.username + '!') });
+            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' doet ook mee!') });
+
+        }
+
+        socket.on('disconnect', function () {
+            clients--;
+
+            var index = users.indexOf(socket.username);
+
+            if (index != -1) {
+
+                users.splice(index, 1);
+            }
+
+            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' is gestopt met het spel!') });
+        });
+    });
+
+    socket.on('msg', function (data) {
+        //Send message to everyone
+        console.log(data);
+        io.sockets.emit('newmsg', data);
+    })
+});
