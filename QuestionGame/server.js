fc9c8ac1689777164b86ffc137b4cbd1b235a610
Ramon Huijg <ryhuijg@outlook.com>
2018-08-07 17:46:04 +0000

    createServer

--- QuestionGame/server.js
+++ QuestionGame/server.js
@@ -1,12 +1,13 @@
 'use strict';
 var app = require('express')();
 var http = require('http').Server(app);
+var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
 
 
 
