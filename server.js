305107a65af033107c9156ed4c8917da19ed395c
Ramon Huijg <ryhuijg@outlook.com>
2018-08-21 15:56:38 +0000

    admin bigscreen

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 
 //init variables
 var questions = [];
-var question1 = { question: "In welk jaar werd de Zuiderzee afgesloten en noemde men het onstane water, het IJsselmeer?", A: "1870", B: "1916", C: "1932", D: "1945", Correct: "C" }
-var question2 = { question: "Waar heeft Flevoland zijn naam aan te danken?", A: "Bij de watersnoodramp van 1916 kwam de familie Flevo om", B: "Johan Flevo (1785-1837) was een pionier op het gebied van drooglegging", C: "De Romeinen noemden het meer dat destijds op die plek lag het Flevomeer", D: "Flevo betekent in het Latijn 'vloed'", Correct: "D" }
-var question3 = { question: "Waarom is Flevoland omgeven door een strook water?", A: "De grond van de omliggende provincies is te zacht", B: "Anders kregen omliggende provincies grondwaterproblemen", C: "Het tussengelegen water dient ter verkoeling voor de toeristen", D: "Ter afbakening van de provinciegrenzen", Correct: "B" }
+var question1 = { question: "In welk jaar werd de Zuiderzee afgesloten en noemde men het onstane water, het IJsselmeer?", type: 'multiple', A: "1870", B: "1916", C: "1932", D: "1945", Correct: "C" }
+var question2 = { question: "Waar heeft Flevoland zijn naam aan te danken?", A: "Bij de watersnoodramp van 1916 kwam de familie Flevo om", type: 'multiple', B: "Johan Flevo (1785-1837) was een pionier op het gebied van drooglegging", C: "De Romeinen noemden het meer dat destijds op die plek lag het Flevomeer", D: "Flevo betekent in het Latijn 'vloed'", Correct: "D" }
+var question3 = { question: "Waarom is Flevoland omgeven door een strook water?", A: "De grond van de omliggende provincies is te zacht", type: 'multiple', B: "Anders kregen omliggende provincies grondwaterproblemen", C: "Het tussengelegen water dient ter verkoeling voor de toeristen", D: "Ter afbakening van de provinciegrenzen", Correct: "B" }
 questions.push(question1);
 questions.push(question2);
 questions.push(question3);
@@ -32,7 +32,7 @@ server.listen(port, () => {
 var io = require('socket.io')(server);
 
 
-var clients = 0;
+var players = 0;
 var users = [];
 
 io.on('connection', function (socket) {
@@ -47,21 +47,26 @@ io.on('connection', function (socket) {
             socket.emit('userExists', username + ' naam bestaat al! Probeer een andere naam.');
         } else {
             users.push(socket.username);
-            clients++;
+            
             if (socket.username == 'admin') {
                 socket.emit('setScreenType', { type: 'admin' })
             }
             else {
+                players++;
                 socket.emit('setScreenType', { type: 'player' })
+                socket.emit('welkommsg', { message: 'Welkom!', username: socket.username });
             }
 
             socket.emit('userSet', { username: socket.username });
-            socket.emit('welkommsg', { message: 'Welkom!', username: socket.username });
-            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' doet ook mee!') });
+            var data = { message: (socket.username + ' doet ook mee!'), players: players.toString() };
+            console.log(data);
+            socket.broadcast.emit('clientconnectmsg', data );
         }
 
         socket.on('disconnect', function () {
-            clients--;
+            if (socket.username != 'admin') {
+                players--;
+            }
 
             var index = users.indexOf(socket.username);
 
@@ -71,7 +76,6 @@ io.on('connection', function (socket) {
             }
 
             console.log(socket.username + ' disconnected.');
-            socket.broadcast.emit('clientconnectmsg', { message: (socket.username + ' is gestopt met het spel!') });
         });
     });
 
@@ -79,5 +83,15 @@ io.on('connection', function (socket) {
         //Send message to everyone
         console.log(data);
         io.sockets.emit('newmsg', data);
+    })
+    socket.on('getQuestion', function (data) {
+        //Send next question to everyone
+        var data = { question: questions[questionselected]  }
+        console.log(data);
+        io.sockets.emit('newquestion', data);
+
+        if (questions.length > questionselected) {
+            questionselected++;
+        }
     })
 });
